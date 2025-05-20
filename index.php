40224ea0eec310a59868fbd4e1ecbe7cd657ab36
Padlian <padlian25@gmail.com>
2025-05-20 08:57:37 +0700

    main comment

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 echo "versi 1";
 
-echo "<p>versi 1 adalah versi pertama</p>"
+echo "<p>versi 1 adalah versi pertama</p>";
+
+echo "<p>versi 1 ini sudah ada fitur 1 nya, tapi masih ada bug</p>";
 ?>
